60c5773a80ae0ab39134974819b2c72595d27302
digitnetx <digitnetx@gmail.com>
2026-08-19 01:07:04 +0300

    Harden WebView lifecycle and SUNMI bridge integration

--- android/app/src/main/java/com/wazi/pos/MainActivity.kt
+++ android/app/src/main/java/com/wazi/pos/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.wazi.pos
 
 import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
@@ -18,15 +16,16 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val APP_URL = "https://wazi-pos.vercel.app"
+        private const val BRIDGE_NAME = "Sunmi"
     }
 
     private lateinit var webView: WebView
     private lateinit var printer: SunmiPrinterManager
+    private lateinit var bridge: SunmiBridge
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         window.statusBarColor = Color.BLACK
         window.navigationBarColor = Color.BLACK
 
@@ -34,8 +33,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(webView)
 
         printer = SunmiPrinterManager(this)
+        bridge = SunmiBridge(printer)
         printer.connect()
 
+        configureWebView()
+        webView.addJavascriptInterface(bridge, BRIDGE_NAME)
+        webView.loadUrl(APP_URL)
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun configureWebView() {
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.settings.databaseEnabled = true
@@ -44,6 +51,8 @@ class MainActivity : AppCompatActivity() {
         webView.settings.allowContentAccess = false
         webView.settings.mediaPlaybackRequiresUserGesture = false
         webView.settings.setSupportZoom(false)
+        webView.settings.builtInZoomControls = false
+        webView.settings.displayZoomControls = false
 
         if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
             WebSettingsCompat.setForceDark(webView.settings, WebSettingsCompat.FORCE_DARK_OFF)
@@ -62,36 +71,31 @@ class MainActivity : AppCompatActivity() {
                 ) {
                     false
                 } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Blocked external navigation",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Toast.makeText(this@MainActivity, "Blocked external navigation", Toast.LENGTH_SHORT).show()
                     true
                 }
             }
         }
-
-        webView.addJavascriptInterface(
-            SunmiBridge(printer),
-            "Sunmi"
-        )
-
-        webView.loadUrl(APP_URL)
     }
 
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
+    override fun onResume() {
+        super.onResume()
+        if (::printer.isInitialized && !printer.connected) {
+            printer.connect()
         }
     }
 
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+    }
+
     override fun onDestroy() {
-        printer.disconnect()
-        webView.removeJavascriptInterface("Sunmi")
-        webView.destroy()
+        if (::webView.isInitialized) webView.removeJavascriptInterface(BRIDGE_NAME)
+        if (::printer.isInitialized) printer.disconnect()
+        if (::webView.isInitialized) {
+            webView.stopLoading()
+            webView.destroy()
+        }
         super.onDestroy()
     }
 }
